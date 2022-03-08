817bf8f39c3eb73979d30616e13355be949d2366
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-08 08:27:35 -0800

    Initial commit

---
+++ echo2.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+int main(){
+	std:: cout << "Hello world";
+	std::cout << "\nI am learning C++";
+	return 0;
+}
---
+++ endl.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout << "Hello using the endl manipulator to print a new line" << endl;
+	cout << "It definitely Works just fine like the backlash n !!";
+	return 0;
+}
