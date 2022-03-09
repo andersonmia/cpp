094559050470b620baf8700d99164f974836d73f
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-09 02:54:03 -0800

    'stringLength.cpp'

---
+++ stringLength.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	cout << "The length or size of the string alphabet is: " << alphabet.length();
+	
+	return 0;
+}
