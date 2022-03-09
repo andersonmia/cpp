d824e9e94f6fd68eceddf64060365111197f4374
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-09 02:49:23 -0800

    'accessString.cpp'

---
+++ accessString.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<String>
+using namespace std;
+
+int main(){
+	int i;
+	string name = "ANDERSON Mia";
+	for(i = 0; i < name.length(); i++){
+		cout << name[i] << "\n";
+	}
+	return 0;
+}
