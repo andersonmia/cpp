a7079c40c804d7f296e4816c93d015369e7b1723
MAJURA Gaudance <96429597+andersonmia@users.noreply.github.com>
2022-03-09 02:53:20 -0800

    'string.cpp'

---
+++ string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main(){
+	string name = "Hello world";
+//	you can also use char to output a string
+char text[] = "This is c++"; 
+	cout << name << "\n" << text;
+	return 0;
+}
